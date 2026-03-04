b0ffd05af72c4dca1c26ec711f5a1fcc21ca2bff
finneymarshal718-prog <finneymarshal718@gmail.com>
2026-03-04 16:30:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -22,5 +22,17 @@ const client = new Client({
 client.once("ready", () => {
   console.log("Bot is online!");
 });
+const prefix = "!";
 
+client.on("messageCreate", message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    message.reply("Pong! 🏓");
+  }
+});
 client.login(process.env.DISCORD_BOT_TOKEN);
